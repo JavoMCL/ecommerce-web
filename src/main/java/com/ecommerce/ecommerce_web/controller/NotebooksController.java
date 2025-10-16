c0f77be2e69deb88513b297e42db646fcd7980de
JavoMCL <hahusussu282828@gmail.com>
2025-10-16 18:41:34 -0300

    Nuevo commit

--- src/main/java/com/ecommerce/ecommerce_web/controller/NotebooksController.java
+++ src/main/java/com/ecommerce/ecommerce_web/controller/NotebooksController.java
@@ -1,12 +1,13 @@
 package com.ecommerce.ecommerce_web.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "main";
-}
+public class NotebooksController {
+    @GetMapping("/notebooks")
+    public String notebooks() {
+        return "notebooks";
+    }
+    
 }
