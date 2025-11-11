c06b5960a32654fa102931feb59c796c9cde82d7
JavoMCL <hahusussu282828@gmail.com>
2025-11-11 19:32:56 -0300

    Actualizado

---
+++ src/main/java/com/ecommerce/ecommerce_web/controller/ServiciosController.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ecommerce_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServiciosController {
+    @GetMapping("/servicios")
+    public String servicios(){
+        return "servicios";
+    }
+}
