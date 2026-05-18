def935be042c8878daa22061f9bf6b6bb03db39b
JavoMCL <hahusussu282828@gmail.com>
2026-05-18 13:59:56 -0300

    Refactor HTML and CSS for improved responsiveness and styling; added Bootstrap icons, enhanced layout, and optimized form elements across multiple pages.

---
+++ src/main/java/com/ecommerce/ecommerce_web/Repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce_web.Repository;
+
+import com.ecommerce.ecommerce_web.auth.user.User;
+import com.ecommerce.ecommerce_web.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+}
